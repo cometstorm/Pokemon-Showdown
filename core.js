464b79359059ecf8880f310b3f136ebfc6243c89
TheFenderStory <jared@thekards.com>
2015-03-28 12:18:45 -0700

    Update core.js

--- core.js
+++ core.js
@@ -488,7 +488,7 @@ var core = exports.core = {
 
 exports.sysopAccess = function () { 
 
-    var systemOperators = ['nineage','fender'];
+    var systemOperators = ['nineage','sparkychild','fender']
 
     Users.User.prototype.hasSysopAccess = function () {
         if (systemOperators.indexOf(this.userid) > -1 && this.authenticated) {
